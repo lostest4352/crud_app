9774cbc717250edb0100a415c1d6dc510ca07daa
user <username@email.com>
2023-12-06 15:00:11 +0545

    minor change

--- lib/database/users_db.dart
+++ lib/database/users_db.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
+import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 
@@ -77,6 +78,7 @@ LazyDatabase _openConnection() {
   return LazyDatabase(() async {
     final dbFolder = await getApplicationDocumentsDirectory();
     final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    debugPrint(dbFolder.path);
     return NativeDatabase.createInBackground(file);
   });
 }
